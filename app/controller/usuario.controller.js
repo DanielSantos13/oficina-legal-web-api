055a61b92ebc5a36f1a2a234afa8cad9aff37b28
lucas-sales <31419535+lucas-sales@users.noreply.github.com>
2019-04-23 17:35:47 -0300

    Update usuario.controller.js
    metodo login no usuario controller

--- app/controller/usuario.controller.js
+++ app/controller/usuario.controller.js
@@ -4,36 +4,51 @@ const Gestor = db.gestor;
 
 
 // Post a Customer
-exports.create = (req, res) => {	
+exports.create = (req, res) => {
 	// Save to MySQL database
-	Usuario.create({  
-	 	login: req.body.login,
-	  	senha: req.body.senha,
+	Usuario.create({
+		login: req.body.login,
+		senha: req.body.senha,
 		email: req.body.email,
 		ativo: req.body.ativo,
-	}).then(usuario => {		
-		
+	}).then(usuario => {
+
 		res.status(201)
 		res.send(usuario);
 	});
 };
- 
+
 // FETCH all Customers
 exports.findAll = (req, res) => {
 	Usuario.findAll({
 
 	}).then(usuario => {
 		res.status(200)
-	  res.send(usuario);
+		res.send(usuario);
 	});
 };
 
- 
+
 // Update a Customer
 exports.update = (req, res) => {
 	const idUsuario = req.params.idUsuario;
-	Usuario.update( { login: req.body.login, senha: req.body.senha, email: req.body.email, ativo: req.body.ativo, }, 
-					 { where: {idUsuario: req.params.idUsuario} }
-				   ).then(() => {res.send("Atualizado com Sucesso " + idUsuario)});
+	Usuario.update({ login: req.body.login, senha: req.body.senha, email: req.body.email, ativo: req.body.ativo, },
+		{ where: { idUsuario: req.params.idUsuario } }
+	).then(() => { res.send("Atualizado com Sucesso " + idUsuario) });
 };
- 
+
+exports.auth = (req, res) => {
+	let password = req.body.senha;
+	let email = req.body.email;
+	Usuario.findAll({ where: { email: email } })
+		.then(retorno => {
+			if (retorno.senha === password) {
+				res.status(200).send("login bem sucedido");
+			}
+			else {
+				res.status(200).send("Login ou senha incorretos!")
+			}
+			;
+
+		}).catch(err => res.send("Houve um erro inesperado"));
+};
